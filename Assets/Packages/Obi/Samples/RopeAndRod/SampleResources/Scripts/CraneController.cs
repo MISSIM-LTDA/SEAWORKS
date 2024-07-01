f33d41adc88e7d1126f309937c3d1fec37c56c6f
MarcoOFonseca <151065509+MarcoOFonseca@users.noreply.github.com>
2024-07-01 14:11:49 -0300

    Fix ROV Prefab

--- Assets/Packages/Obi/Samples/RopeAndRod/SampleResources/Scripts/CraneController.cs
+++ Assets/Packages/Obi/Samples/RopeAndRod/SampleResources/Scripts/CraneController.cs
@@ -1,40 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Obi;
 
 public class CraneController : MonoBehaviour
 {
+    private ObiRope rope;
+    private ObiRopeCursor cursor;
 
-	ObiRopeCursor cursor;
-	ObiRope rope;
-	public KeyCode Pagar;
-	public KeyCode Recolher;
-	public float speed1;
+	public KeyCode extend;
+	public KeyCode decrease;
 
+	public float speed;
 
-	// Use this for initialization
 	void Start()
 	{
 		cursor = GetComponentInChildren<ObiRopeCursor>();
 		rope = cursor.GetComponent<ObiRope>();
 	}
-
-	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
-		if (Input.GetKey(Recolher))
-		{
-			if (rope.restLength > 6.5f)
-				cursor.ChangeLength(rope.restLength - speed1 * Time.deltaTime);
+		if (Input.GetKey(decrease) && rope.restLength > 6.5f){
+			cursor.ChangeLength(rope.restLength - speed * Time.deltaTime);
 		}
 
-		if (Input.GetKey(Pagar))
-		{
-			
-			cursor.ChangeLength(rope.restLength + speed1 * Time.deltaTime);
+		else if (Input.GetKey(extend)){
+			cursor.ChangeLength(rope.restLength + speed * Time.deltaTime);
 		}
-
 
 	}
 }
